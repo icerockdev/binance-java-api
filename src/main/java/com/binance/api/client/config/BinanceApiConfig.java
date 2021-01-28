9903f786709daa42dc372c4ebb74586a062371d5
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2021-01-28 22:20:43 +0700

    allow to change config logic

--- src/main/java/com/binance/api/client/config/BinanceApiConfig.java
+++ src/main/java/com/binance/api/client/config/BinanceApiConfig.java
@@ -4,49 +4,49 @@ package com.binance.api.client.config;
  * Configuration used for Binance operations.
  */
 public class BinanceApiConfig {
+    public String baseDomain = "binance.com";
 
-	/**
-	 * Base domain for URLs.
-	 */
-	private static String BASE_DOMAIN = "binance.com";
+    public String apiBaseUrl() {
+        return String.format("https://api.%s", baseDomain);
+    }
 
-	/**
-	 * Set the URL base domain name (e.g., binance.com).
-	 *
-	 * @param baseDomain Base domain name
-	 */
-	public static void setBaseDomain(final String baseDomain) {
-		BASE_DOMAIN = baseDomain;
-	}
+    public String streamApiBaseUrl() {
+        return String.format("wss://stream.%s:9443/ws", baseDomain);
+    }
 
-	/**
-	 * Get the URL base domain name (e.g., binance.com).
-	 *
-	 * @return The base domain for URLs
-	 */
-	public static String getBaseDomain() {
-		return BASE_DOMAIN;
-	}
+    public String assetInfoApiBaseUrl() {
+        return String.format("https://%s/", baseDomain);
+    }
 
-	/**
-	 * REST API base URL.
-	 */
-	public static String getApiBaseUrl() {
-		return String.format("https://api.%s", getBaseDomain());
-	}
+    public static BinanceApiConfig instance = new BinanceApiConfig();
 
-	/**
-	 * Streaming API base URL.
-	 */
-	public static String getStreamApiBaseUrl() {
-		return String.format("wss://stream.%s:9443/ws", getBaseDomain());
-	}
+    /**
+     * Get the URL base domain name (e.g., binance.com).
+     *
+     * @return The base domain for URLs
+     */
+    public static String getBaseDomain() {
+        return instance.baseDomain;
+    }
 
-	/**
-	 * Asset info base URL.
-	 */
-	public static String getAssetInfoApiBaseUrl() {
-		return String.format("https://%s/", getBaseDomain());
-	}
+    /**
+     * REST API base URL.
+     */
+    public static String getApiBaseUrl() {
+        return instance.apiBaseUrl();
+    }
 
+    /**
+     * Streaming API base URL.
+     */
+    public static String getStreamApiBaseUrl() {
+        return instance.streamApiBaseUrl();
+    }
+
+    /**
+     * Asset info base URL.
+     */
+    public static String getAssetInfoApiBaseUrl() {
+        return instance.assetInfoApiBaseUrl();
+    }
 }
